bc1f73fa2d1556c2da024086e04ffeaa0c948a9d
Arteen Abrishami <arteen1000@gmail.com>
2023-02-10 00:37:16 -0800

    react implementation

---
+++ simply-tasks/src/application.js
@@ -0,0 +1,25 @@
+function App() {
+  return (
+    <>
+      <NavBar />
+    </>
+  );
+}
+
+function NavBar() {
+  return (
+  <div className="navbar"> 
+      <div className="container">
+        <div className="logo">Simply<span>Tasks</span></div>
+
+        <nav>
+            <ul>
+                <li className="log-out"><a href="home-page/home-page.html">Log Out</a></li>
+            </ul>
+        </nav>
+      </div>
+  </div>
+  );
+}
+
+export default App;
