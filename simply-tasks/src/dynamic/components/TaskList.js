3b04e24d418cfa83c2e2747a941a6027c05e56ae
Arteen Abrishami <arteen1000@gmail.com>
2023-02-12 13:07:58 -0800

    finished project

--- simply-tasks/src/dynamic/components/TaskList.js
+++ simply-tasks/src/dynamic/components/TaskList.js
@@ -2,6 +2,7 @@ import { useState } from 'react'
 
 import TaskListHeader from './TaskListHeader'
 import Tasks from './Tasks'
+import TaskAdder from './TaskAdder'
 
 export default TaskList;
 
@@ -11,22 +12,26 @@ function TaskList () {
         {
           id: 0,
           content: 'finish task list',
-          date: 'this week',
+          date: '2023-02-12',
           highlight: true
         },
         {
           id: 1,
           content: 'finish add functionality',
-          date: 'this week',
+          date: '2023-02-12',
           highlight: false
         },
         {
           id: 2,
           content: 'make add functionality look really good',
-          date: 'this week',
+          date: '2023-02-12',
           hightlight: false
         }
       ]);
+
+      // will show TaskAdder
+      const [showAdder, setShowAdder] = useState(false);
+
     
       // delete task
       const deleteTask = (e, id) => {
@@ -44,11 +49,23 @@ function TaskList () {
         )
       }
 
+      // add task
+      const addTask = (task) => {
+        // TODO: random number, replace with the database ID 
+        // this will break at some point if enough tasks are added
+        const id = Math.floor(Math.random() * 11111) + 3;
+        const highlight = false;
+
+        const newTask = { id, ...task, highlight };
+        setTasks([newTask, ...tasks]);
+      }
+
     return (
         <>
             <div className="task-list">
                 <div className="container">
-                <TaskListHeader />
+                <TaskListHeader setAdder={() => setShowAdder(true)} />
+               { showAdder && <TaskAdder addTask={addTask} unsetAdder={() => setShowAdder(false)} /> }
                 {tasks.length > 0 ? <Tasks tasks={tasks} highlightTask={highlightTask} deleteTask={deleteTask} />: <div className="no-tasks">Empty Task List</div>}
                 </div>
             </ div>
