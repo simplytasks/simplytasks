ed4c75df3ecbb1705a56ab92e1981e2c6c9e22d6
IssaLaptop <ma42202@hotmail.com>
2023-02-12 20:20:54 -0800

    initial firebase stuff

---
+++ simply-tasks/backend/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+//Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCwFIfdHggjYB5ro0FF96N93pVpA0ER-i4",
+  authDomain: "simply-tasks-backend.firebaseapp.com",
+  projectId: "simply-tasks-backend",
+  storageBucket: "simply-tasks-backend.appspot.com",
+  messagingSenderId: "570295948312",
+  appId: "1:570295948312:web:2305e3f4cbd63d7ea19f6d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
