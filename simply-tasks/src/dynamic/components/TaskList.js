e19bc289b72172730f56ca8e6ab8df44739717d3
Byron Karlen <byronkarlen@gmail.com>
2023-02-19 15:07:11 -0600

    Add sort by message feature
    I added a button element which, when clicked, allows the tasks to be sorted (in roughly alphabetic order) by the message
    associated with a given task. This allows users to keep their tasks more organized.

--- simply-tasks/src/dynamic/components/TaskList.js
+++ simply-tasks/src/dynamic/components/TaskList.js
@@ -31,6 +31,32 @@ function TaskList () {
 
       // will show TaskAdder
       const [showAdder, setShowAdder] = useState(false);
+      // keeps track of how tasks will be sorted
+      const [sortMethod, setSortMethod] = useState('None');
+
+      const sortTasksByMessage = (task1, task2) => {
+        if(tasks.length < 2) return;
+        if(task1.content < task2.content) return -1;
+        else if (task1.content > task2.content) return 1;
+        else return 0; 
+      }
+      
+      const sortTasks = (currentTasks) => {
+        if(sortMethod === 'Message')
+          currentTasks.sort(sortTasksByMessage);
+        //implement other if statements here
+        setTasks(currentTasks);
+      }
+
+      const changeSort = () => {
+        let newSortMethod = 'Message';
+        // if(sortMethod === 'None') 
+        //   newSortMethod = 'Message'
+        // else if(sortMethod === 'Message')
+        //   newSortMethod = 'None'
+        setSortMethod(newSortMethod);
+        sortTasks(tasks);
+      }
 
     
       // delete task
@@ -57,15 +83,17 @@ function TaskList () {
         const highlight = false;
 
         const newTask = { id, ...task, highlight };
-        setTasks([newTask, ...tasks]);
+        let currentTasks = [newTask, ...tasks]
+        setTasks(currentTasks);
+        sortTasks(currentTasks);
       }
 
     return (
         <>
             <div className="task-list">
                 <div className="container">
-                <TaskListHeader setAdder={() => setShowAdder(true)} />
-               { showAdder && <TaskAdder addTask={addTask} unsetAdder={() => setShowAdder(false)} /> }
+                <TaskListHeader setAdder={() => setShowAdder(true)} changeSort={() => changeSort()}/>
+                {showAdder && <TaskAdder addTask={addTask} unsetAdder={() => setShowAdder(false)} /> }
                 {tasks.length > 0 ? <Tasks tasks={tasks} highlightTask={highlightTask} deleteTask={deleteTask} />: <div className="no-tasks">Empty Task List</div>}
                 </div>
             </ div>
--- simply-tasks/src/dynamic/components/TaskListHeader.js
+++ simply-tasks/src/dynamic/components/TaskListHeader.js
@@ -1,12 +1,12 @@
 import Button from './Button'
 
-const TaskListHeader = ({setAdder}) => {
+const TaskListHeader = ({setAdder, changeSort}) => {
 
     return (
         <header className='header'>
         <div className="logo">Simply<span>Tasks</span></div>
-        <Button contents='Add Task' 
-        handleClick={setAdder} />
+        <Button contents='Sort By Message' handleClick={changeSort} />
+        <Button contents='Add Task' handleClick={setAdder} />
         </header>
     );
 }
