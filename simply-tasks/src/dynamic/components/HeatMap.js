758a3579e8b196fd71d168683e88f37d32547ec0
nakkstar123 <nakulkhambhati@g.ucla.edu>
2023-02-25 18:22:16 -0800

    highlight buttons conditionally, does not use current task list though

--- simply-tasks/src/dynamic/components/HeatMap.js
+++ simply-tasks/src/dynamic/components/HeatMap.js
@@ -3,28 +3,14 @@ import Calendar from 'react-calendar';
 
 import TaskListHeader from './TaskListHeader'
 
-import { differenceInCalendarDays } from 'date-fns';
-
-const isSameDay = a => b => {
-  return differenceInCalendarDays(a, b) === 0;
-}
-
-const datesToAddClassTo = ['2023-02-12'];
-
-function tileClassName({ date, view }) {
-    // Add class to tiles in month view only
-    if (view === 'month') {
-      // Check if a date React-Calendar wants to check is on the list of dates to add class to
-      if (datesToAddClassTo.find(dDate => isSameDay(dDate, date))) {
-        return 'styleButtons';
-      }
-    }
-  }
+const manyTasks = ['01-02-2023', '16-02-2023']; // dates with more than 5 tasks
+const someTasks = ['14-02-2023', '04-02-2023']; // dates with 3-5 tasks
+const fewTasks = ['30-01-2023', '24-02-2023', '26-02-2023']; // dates with 1 or 2 tasks
 
 export default HeatMap;
 // remove add task in the header here 
 function HeatMap(){
-    const [date, setDate] = useState(new Date());
+    const [date, setDate] = useState(new Date()); 
 
     return (
         <>
@@ -35,7 +21,29 @@ function HeatMap(){
                 <div className='calendar'>
 
                     <div className='calendar-container'>
-                        <Calendar onChange={setDate} value={date} className={()=>tileClassName('2023-02-12', 'month')}/>
+                        <Calendar onChange={setDate} value={date}
+                        tileClassName={({date})=>{
+                          let day = date.getDate()
+                          let month = date.getMonth() + 1
+                          if (day < 10){
+                            day = '0'+day
+                          }
+                          if (month < 10){
+                            month = '0'+month
+                          }
+                          const realDate = day+'-'+month+'-'+date.getFullYear()
+                          if (manyTasks.find(val=>val===realDate)){
+                            return 'highlight3'
+                          }
+                          else if (someTasks.find(val=>val===realDate)){
+                            return 'highlight2'
+                          }
+                          else if (fewTasks.find(val=>val===realDate)){
+                            return 'highlight1'
+                          }
+                        }
+                        }
+                        />
                     </div>
                     </div>
 
